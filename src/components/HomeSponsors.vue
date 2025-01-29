e471e921393f11bf39f52d294fd15a42e304627d
Leo Chuang <chuangl@garena.com>
2025-01-29 19:05:51 +0800

    upd: update marquee animation for sponsors

--- src/components/HomeSponsors.vue
+++ src/components/HomeSponsors.vue
@@ -5,50 +5,110 @@
         <div class="sponsors__title">
           {{ $t('home.sponsors_title') }}
         </div>
-        <div class="sponsors-list">
-          <div class="sponsors-item" v-for="item in 24" :key="item">{{ item }}</div>
+        <div class="sponsors-row">
+          <div class="sponsors-list">
+            <div
+              class="sponsors-item"
+              v-for="(item, idx) in sponsors"
+              :key="item"
+              :style="{ backgroundImage: `url(${item})` }"
+            ></div>
+            <div
+              class="sponsors-item"
+              v-for="(item, idx) in sponsors"
+              :key="item"
+              :style="{ backgroundImage: `url(${item})` }"
+            ></div>
+          </div>
+        </div>
+        <div class="sponsors-row">
+          <div class="sponsors-list sponsors-list--reverse">
+            <div
+              class="sponsors-item"
+              v-for="item in sponsors"
+              :key="item"
+              :style="{ backgroundImage: `url(${item})` }"
+            ></div>
+            <div
+              class="sponsors-item"
+              v-for="item in sponsors"
+              :key="item"
+              :style="{ backgroundImage: `url(${item})` }"
+            ></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const sponsors = Array(8)
+  .fill(0)
+  .map(() => {
+    return `https://fakeimg.pl/176x144/`
+  })
+</script>
 <style lang="scss" scoped>
 .home-sponsors {
   margin: auto;
   margin-bottom: 2.16rem;
-  .sponsors-container {
-    @include withContainer;
-  }
   .sponsors {
     width: 13.6rem;
-    padding: 0.64rem;
+    padding: 0.64rem 0;
     margin: 0 auto;
     border-radius: 0.1rem;
     background-color: #fff;
   }
   .sponsors__title {
+    @include withContainer;
+    padding: 0 0.64rem;
     color: #000;
     font-size: 0.48rem;
     font-weight: 700;
     margin-bottom: 0.32rem;
   }
-  .sponsors-list {
+  .sponsors-row {
+    overflow: hidden;
+    white-space: nowrap;
     display: flex;
-    align-items: center;
-    width: 100%;
-    justify-content: flex-start;
-    flex-wrap: wrap;
+    &:not(:last-child) {
+      margin-bottom: 0.4rem;
+    }
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+  .sponsors-list {
+    white-space: nowrap;
+    @include flexCenter;
     gap: 0.32rem;
+    animation: marquee 15s linear infinite;
+    &:hover {
+      animation-play-state: paused;
+    }
+    &--reverse {
+      animation-direction: reverse;
+    }
+
     .sponsors-item {
+      flex-shrink: 0;
       background-color: #f5f5f5;
       border-radius: 0.08rem;
       @include flexCenter;
+      @include bgCenter(cover);
       color: #000;
-      width: 1.72rem;
-      height: 1.43rem;
+      width: 1.76rem;
+      height: 1.44rem;
     }
+  }
+}
+@keyframes marquee {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-50%);
   }
 }
 </style>

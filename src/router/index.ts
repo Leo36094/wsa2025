4fbd967b0ccf88669e39523c9917990c20b3d1a0
Leo Chuang <chuangl@garena.com>
2025-01-30 07:17:54 +0800

    upd: update change route scroll behavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,6 +23,13 @@ const router = createRouter({
       component: () => import('../views/CompetitionView.vue'),
     }
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0, behavior: 'instant' }
+    }
+  }
 })
 
 export default router

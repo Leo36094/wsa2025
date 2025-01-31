0562c7e893460189d36a52c8ec421bf07a797144
Leo Chuang <chuangl@garena.com>
2025-01-31 13:07:45 +0800

    Updates

---
+++ assets/SkillsLogisticView-BcvuRWk3.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./index-Dexbmeh1.js";const t={};function e(r,s){return"Logistic"}const o=c(t,[["render",e]]);export{o as default};
--- assets/SkillsLogisticView-Drpflmqq.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./index-DRMv_Cf-.js";const t={};function e(r,s){return"Logistic"}const o=c(t,[["render",e]]);export{o as default};

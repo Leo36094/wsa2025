20589b6b520e39d6ee4040599a63323d7f24ee69
Leo Chuang <chuangl@garena.com>
2025-01-31 16:33:11 +0800

    Updates

--- assets/SkillsInformationView-CjsWF-Wv.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as n,a as s,o as a}from"./index-Dexbmeh1.js";const c={},t={class:"skills-information"};function i(l,o){return a(),n("div",t,o[0]||(o[0]=[s("div",{class:"container"},[s("div",{class:"row"},[s("div",{class:"col"},[s("h1",null,"Skills Information Tech")])])],-1)]))}const d=e(c,[["render",i]]);export{d as default};

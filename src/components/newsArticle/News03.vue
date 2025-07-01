754f5300ba5f84b08b5fc387cc28ed7c13c3e2da
Leo Chuang <chuangl@garena.com>
2025-07-02 02:13:37 +0800

    upd: update news03 UI

--- src/components/newsArticle/News03.vue
+++ src/components/newsArticle/News03.vue
@@ -1,6 +1,28 @@
 <template>
-  <GetInvolvedTour />
+  <div class="section-container">
+    <GetInvolvedTour class="news-03" />
+  </div>
 </template>
 <script setup lang="ts">
 import GetInvolvedTour from '@/components/GetInvolvedTour.vue'
 </script>
+
+<style lang="scss" scoped>
+.section-container {
+  @include flexCenter(column);
+  width: 6.24rem;
+  margin: 0 auto;
+}
+
+@include tablet {
+  .section-container {
+    width: 6.64rem;
+  }
+}
+@include mobile {
+  .section-container {
+    width: 100%;
+    padding: 0.16rem;
+  }
+}
+</style>

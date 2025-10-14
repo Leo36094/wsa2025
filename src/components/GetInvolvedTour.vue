d95f43838f32ba4cbf031ba2b92516e5759d62f8
Leo Chuang <chuangl@garena.com>
2025-10-14 21:08:00 +0800

    upd: update tourguide images and adjust i18n

--- src/components/GetInvolvedTour.vue
+++ src/components/GetInvolvedTour.vue
@@ -19,9 +19,9 @@ const { locale } = useI18n()
 
 const dmImg = computed(() => {
   if (locale.value === 'en') {
-    return import.meta.env.BASE_URL + 'images/getInvolved/tour/tour_dm_en.png'
+    return import.meta.env.BASE_URL + 'images/getInvolved/tour/tour_dm_en_1014.png'
   }
-  return import.meta.env.BASE_URL + 'images/getInvolved/tour/tour_dm_zh.png'
+  return import.meta.env.BASE_URL + 'images/getInvolved/tour/tour_dm_zh_1014.png'
 })
 const visaMethodList = computed(() => [
   {
